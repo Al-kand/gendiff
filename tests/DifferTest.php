32875fa54b087463338c604457c471ea33fce72c
Alkand <alkand@mysite.com>
2022-05-29 13:07:49 +0300

    add yaml parse

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -4,47 +4,43 @@ namespace Differ\Tests;
 
 use PHPUnit\Framework\TestCase;
 
-use function Differ\Differ\readFile;
+use function Differ\Parsers\parse;
 use function Differ\Differ\genDiff;
 
 class DifferTest extends TestCase
 {
     protected $files;
+    protected $result;
 
     protected function setUp(): void
     {
         $this->files = [
             'json1' => 'tests/fixtures/file1.json',
-            'json2' => 'tests/fixtures/file2.json'
+            'json2' => 'tests/fixtures/file2.json',
+            'yaml1' => 'tests/fixtures/file1.yaml',
+            'yaml2' => 'tests/fixtures/file2.yml'
         ];
+        $this->result = file_get_contents('tests/fixtures/result');
     }
 
-    public function testreadFile()
+    public function testParse()
     {
-        $expected = [
+        $expected = (object) [
             "host" => "hexlet.io",
             "timeout" => 50,
             "proxy" => "123.234.53.22",
             "follow" => false
         ];
 
-        $this->assertEquals($expected, readFile($this->files['json1']));
+        $this->assertEquals($expected, parse($this->files['json1']));
+        $this->assertEquals($expected, parse($this->files['yaml1']));
     }
 
-    public function testgenDiff()
+    public function testGenDiff()
     {
-        $expected = <<<EXP
-        {
-          - follow: false
-            host: hexlet.io
-          - proxy: 123.234.53.22
-          - timeout: 50
-          + timeout: 20
-          + verbose: true
-        }
-        
-        EXP;
+        $expected = $this->result;
 
         $this->assertEquals($expected, genDiff($this->files['json1'], $this->files['json2']));
+        $this->assertEquals($expected, genDiff($this->files['yaml1'], $this->files['yaml2']));
     }
 }
