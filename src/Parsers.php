09093c0e98902462dad5d72c8c87480ec8b59ef3
Aleks <my@mail.com>
2022-05-31 00:33:24 +0300

    make tests for nested structures

--- src/Parsers.php
+++ src/Parsers.php
@@ -8,16 +8,14 @@ function parse(string $path): object
 {
     $content = file_get_contents($path);
     $type = pathinfo($path, PATHINFO_EXTENSION);
+
     switch ($type) {
         case 'json':
             return json_decode($content, false);
-            break;
         case 'yaml':
         case 'yml':
             return Yaml::parse($content, Yaml::PARSE_OBJECT_FOR_MAP);
-            break;
         default:
             throw new \Exception("Unknown file type {$type}");
-            break;
     }
 }
